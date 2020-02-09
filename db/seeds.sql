9a55f5b3e84c0f3247c5938117d4005318ea9bc8
Mandy Torrence <mandolinhaynes@gmail.com>
2020-02-08 22:35:03 -0500

    Adding the MySQL databases

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (name) VALUES ('Administration');
+INSERT INTO wishes (wish) VALUES ('Bruce wants to be a rich superhero.');
+INSERT INTO wishes (wish) VALUES ('Mary wants to keep on rollin');
